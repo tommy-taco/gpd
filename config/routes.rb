fdebf5c9410d23857cd690c2f410dc08c9c9d538
Prelang Builder <builder@prelang.com>
2014-07-04 03:29:17 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'statics/index'
+
   resources :goals
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
