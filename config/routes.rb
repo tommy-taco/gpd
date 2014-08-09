64def21a536c1dc240e492c43a207c9dfdc7f5d7
AJ <ajhoward14@gmail.com>
2014-08-09 15:03:41 -0700

    no errors :/

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   get 'users/show'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
@@ -8,10 +10,6 @@ Rails.application.routes.draw do
 	match '/contact', to: 'statics#contact', via: 'get'
 	match '/upload', to: 'statics#upload', via: 'get'
 	match '/gtest', to: 'statics#gtest', via: 'get'
-
-  unless Rails.application.config.consider_all_requests_local
-    match '*not_found', to: 'errors#error_404'
-  end
 
 
 	resources :goals do
