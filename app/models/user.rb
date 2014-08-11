55d55c37aa6714c9831f9025945a553899b2bfc3
AJ <ajhoward14@gmail.com>
2014-08-11 15:44:48 -0700

    add goals and user team fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  belongs_to :team, :class_name => 'Team'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
